68b4a651ed5379c2b7021c7d183870b50fbe4e80
beth nduta <bethnduta05@gmail.com>
2022-05-28 15:11:29 +0300

    defined test method in the models

--- photos/models.py
+++ photos/models.py
@@ -7,6 +7,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+    def 
+    
 class Photo(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(null=False, blank=False)
